5a340095cee0cf2b8133f461c114d81bd803f444
brunaiis2013pro <brunaiis2013pro@gmail.com>
2020-09-10 12:21:31 -0400

    practicaa1

---
+++ src/Main.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args){
+        Practica1 p1 = new Practica1();
+
+        p1.ejecutar();
+        p1.mostrar();
+
+    }
+
+
+
+}
--- src/Practica1.java
+++ src/Practica1.java
@@ -1,36 +1,34 @@
+import java.util.Scanner;
+
 /*
 Ejercicio 3
 Se quiere realizar un programa que lea por teclado las 5 notas obtenidas por un alumno (comprendidas
 entre 0 y 10). A continuación debe mostrar todas las notas, la nota media, la nota más alta que ha
 sacado y la menor.
  */
-package practica1;
+public class Practica1 {
+    Scanner entrada = new Scanner(System.in);
 
-import java.util.Scanner;
+    int[] notas = new int[5];
+    double suma=0,promedio;
+    int mayor=0,menor=999999;
 
-public class PRACTICA1 {
+    //Metodo ejecutar
+    public  void ejecutar(){
 
-    public static void main(String[] args) {
-         Scanner entrada = new Scanner(System.in);
-        
-        int[] notas = new int[5];
-        double suma=0,promedio;
-        int mayor=0,menor=999999;
-        
-        
         //Pedimos las 5 notas del alumno
         System.out.println("\t\tNotas comprendidas entre [0-10]\n");
         for(int i=0; i<notas.length; i++){
-            
+
             //Controlamos que estee en dicho rango
             do{
                 System.out.print("Digite la nota "+(i+1)+": ");
                 notas[i] = entrada.nextInt();
             }while(notas[i]<0 || notas[i]>10);
-           
-            
+
+
             suma+=notas[i]; //Sumamos todas las notas
-            
+
             if(notas[i]>mayor){//La nota mayor
                 mayor = notas[i];
             }
@@ -39,18 +37,23 @@ public class PRACTICA1 {
             }
         }
         promedio = suma /notas.length;
-        
-        //Mostramos todas las notas del alumno
+
+    }
+
+    //Mostramos todas las notas del alumno
+    public void mostrar(){
+
         System.out.print("\nNOTAS: ");
         for(int i=0; i<notas.length; i++){
             System.out.print(notas[i]+" ");
         }
         System.out.println();
-        
-        
+
+
         System.out.println("La nota media es: "+promedio);
         System.out.println("La nota mayor es: "+mayor);
         System.out.println("La nota menor es: "+menor);
     }
-    
+
+
 }
